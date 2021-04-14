3b11c4f7483dfa75286dbdc7c36011bd9de59526
ACLTearr <burroughskaelen@gmail.com>
2021-04-14 12:47:41 -0400

    Commit

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Drizzle } from '@drizzle/store'
+import { DrizzleContext } from '@drizzle/react-plugin'
 import Greeter from './artifacts/Greeter.json'
 import './App.css'
 
@@ -12,13 +14,11 @@ const greeter = new Drizzle(options)
 
 function App() {
   return (
-    <DrizzleProvider options={greeter}>
       <div className="App">
         <header className="App-header">
           <h2>Hello World!</h2>
         </header>
       </div>
-    </DrizzleProvider>
   );
 }
 
