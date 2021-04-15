99c8cc23b7f547f2c942a7497232c4bdda2431bf
Ahsan Ali Syed <ahsantime1@gmail.com>
2021-04-15 02:38:42 -0400

    pulled in projectoffice

---
+++ contracts/ProjectOffice.sol
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+pragma experimental ABIEncoderV2;
+
+contract ProjectOffice {
+  Parts[] parts;
+  // address public projects;
+  // address public owner;
+  address[10] public projects;
+
+  // event for EVM logging
+  // event OwnerSet(address indexed oldOwner, address indexed newOwner);
+  function assign(uint projectId) public returns (uint) {
+      require(projectId >= 0 && projectId <= 9);
+
+      projects[projectId] = msg.sender;
+
+      return projectId;
+  }
+
+  struct Parts {
+    uint256 Controllers;
+    uint256 Shafts;
+    uint256 Doors;
+    uint256 Buttons;
+    uint256 Displays;
+    uint256 Pulleys;
+  }
+
+  // modifier isOwner() {
+  //       // If the first argument of 'require' evaluates to 'false', execution terminates and all
+  //       // changes to the state and to Ether balances are reverted.
+  //       // This used to consume all gas in old EVM versions, but not anymore.
+  //       // It is often a good idea to use 'require' to check if functions are called correctly.
+  //       // As a second argument, you can also provide an explanation about what went wrong.
+  //       require(msg.sender == owner, "Caller is not owner");
+  //       _;
+  // }
+
+  // constructor() {
+  //     owner = msg.sender; // 'msg.sender' is sender of current call, contract deployer for a constructor
+  //     emit OwnerSet(address(0), owner);
+  // }
+
+  // function changeOwner(address newOwner) public isOwner {
+  //       emit OwnerSet(owner, newOwner);
+  //       owner = newOwner;
+  // }
+
+  function neededParts(uint256 Batteries, uint256 Columns, uint256 Elevators, uint256 Floors) public {
+      Parts memory p;
+      p.Controllers = Batteries;
+      p.Shafts = Elevators;
+      p.Doors = Elevators * Floors * Columns;
+      p.Buttons = Elevators * Floors;
+      p.Displays = Elevators;
+      p.Pulleys = Elevators;
+
+      parts.push(p);
+  }
+
+  function getAddresses() public view returns (address[10] memory) {
+    return projects;
+  }
+
+  function getProjects() public view returns (Parts[] memory) {
+    return parts;
+  }
+}
