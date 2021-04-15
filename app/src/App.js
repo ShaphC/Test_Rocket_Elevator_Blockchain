15efca4fe2a4e2fd09dcd7e4cf31abc2ad9609f8
ACLTearr <burroughskaelen@gmail.com>
2021-04-15 13:09:10 -0400

    reconfigured gitignore

--- app/src/App.js
+++ app/src/App.js
@@ -31,22 +31,87 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello, World!</h1>
-        <p>Your account: {this.state.account}</p>
-        <p>Greeter address: {this.state.address}</p>
+        <div className="container-fluid">
+          <div className="row">
+            <h1 className="col-lg-12 d-flex justify-content-center">Rocket Elevators Blockchain Contracts</h1>
+
+            <br />
+            <br />
+            <br />
+            <br />
+
+            <h2 className="col-lg-12 d-flex justify-content-center">Project Office Contract</h2>
+            <main role="main" className="col-lg-12 d-flex justify-content-center">
+              <div id="content">
+                <form>
+                  <input id="battery" type="text" className="form-control" placeholder="Enter the number of batteries required" required />
+                  <br />
+                  <input id="column" type="text" className="form-control" placeholder="Enter the number of columns required" required />
+                  <br />
+                  <input id="elevator" type="text" className="form-control" placeholder="Enter the number of elevators required" required />
+                  <br />
+                  <input id="floorsServed" type="text" className="form-control" placeholder="Enter the number of floors to be served" required />
+                  <br />
+                  <button className='btn btn-primary col-lg-12 d-flex justify-content-center' type='button'>Submit</button>
+                </form>
+              </div>
+            </main>
+          </div>
+        </div>
+
+        <br />
+        <br />
 
         <div className="container-fluid">
           <div className="row">
+            <h2 className="col-lg-12 d-flex justify-content-center">Material Provider Contract</h2>
             <main role="main" className="col-lg-12 d-flex justify-content-center">
               <div id="content">
                 <form>
                   <input id="newGreeting" type="text" className="form-control" placeholder="Add greeting" required />
-                  <button className='btn btn-primary' type='button'>Submit</button>
+                  <br />
+                  <button className='btn btn-primary col-lg-12 d-flex justify-content-center' type='button'>Submit</button>
                 </form>
               </div>
             </main>
+          </div>
         </div>
-      </div>
+
+        <br />
+        <br />
+
+        <div className="container-fluid">
+          <div className="row">
+            <h2 className="col-lg-12 d-flex justify-content-center">Solution Manufacturing Contract</h2>
+            <main role="main" className="col-lg-12 d-flex justify-content-center">
+              <div id="content">
+                <form>
+                  <input id="newGreeting" type="text" className="form-control" placeholder="Add greeting" required />
+                  <br />
+                  <button className='btn btn-primary col-lg-12 d-flex justify-content-center' type='button'>Submit</button>
+                </form>
+              </div>
+            </main>
+          </div>
+        </div>
+
+        <br />
+        <br />
+
+        <div className="container-fluid">
+          <div className="row">
+            <h2 className="col-lg-12 d-flex justify-content-center">Quality, Security, and Homologation Contract</h2>
+            <main role="main" className="col-lg-12 d-flex justify-content-center">
+              <div id="content">
+                <form>
+                  <input id="newGreeting" type="text" className="form-control" placeholder="Add greeting" required />
+                  <br />
+                  <button className='btn btn-primary col-lg-12 d-flex justify-content-center' type='button'>Submit</button>
+                </form>
+              </div>
+            </main>
+          </div>
+        </div>
       </div>
     );
   }
