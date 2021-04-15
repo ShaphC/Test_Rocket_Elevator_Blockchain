8fe74d01c981e0c1da1a40b838236ae496dac813
ShaphanCharles <web_shaph@hotmail.com>
2021-04-15 11:15:12 -0400

    ignore build test

--- contracts/ProjectOffice.sol
+++ contracts/ProjectOffice.sol
@@ -4,19 +4,19 @@ pragma experimental ABIEncoderV2;
 
 contract ProjectOffice {
   Parts[] parts;
-  // address public projects;
-  // address public owner;
-  address[10] public projects;
+  address public owner;
+  // mapping(address => Projects) public projects;
 
   // event for EVM logging
-  // event OwnerSet(address indexed oldOwner, address indexed newOwner);
-  function assign(uint projectId) public returns (uint) {
-      require(projectId >= 0 && projectId <= 9);
+  event OwnerSet(address indexed oldOwner, address indexed newOwner);
 
-      projects[projectId] = msg.sender;
+  // function assign(uint projectId) public returns (uint) {
+  //     require(projectId >= 0 && projectId <= 9);
 
-      return projectId;
-  }
+  //     projects[projectId] = msg.sender;
+
+  //     return projectId;
+  // }
 
   struct Parts {
     uint256 Controllers;
@@ -27,20 +27,22 @@ contract ProjectOffice {
     uint256 Pulleys;
   }
 
-  // modifier isOwner() {
-  //       // If the first argument of 'require' evaluates to 'false', execution terminates and all
-  //       // changes to the state and to Ether balances are reverted.
-  //       // This used to consume all gas in old EVM versions, but not anymore.
-  //       // It is often a good idea to use 'require' to check if functions are called correctly.
-  //       // As a second argument, you can also provide an explanation about what went wrong.
-  //       require(msg.sender == owner, "Caller is not owner");
-  //       _;
-  // }
+  // event DataStored(uint256 Controllers);
 
-  // constructor() {
-  //     owner = msg.sender; // 'msg.sender' is sender of current call, contract deployer for a constructor
-  //     emit OwnerSet(address(0), owner);
-  // }
+  //  modifier isOwner() {
+  //        // If the first argument of 'require' evaluates to 'false', execution terminates and all
+  //        // changes to the state and to Ether balances are reverted.
+  //        // This used to consume all gas in old EVM versions, but not anymore.
+  //        // It is often a good idea to use 'require' to check if functions are called correctly.
+  //        // As a second argument, you can also provide an explanation about what went wrong.
+  //        require(msg.sender == owner, "Caller is not owner");
+  //        _;
+  //  }
+
+   constructor() {
+       owner = msg.sender; // 'msg.sender' is sender of current call, contract deployer for a constructor
+       emit OwnerSet(address(0), owner);
+   }
 
   // function changeOwner(address newOwner) public isOwner {
   //       emit OwnerSet(owner, newOwner);
@@ -51,17 +53,17 @@ contract ProjectOffice {
       Parts memory p;
       p.Controllers = Batteries;
       p.Shafts = Elevators;
-      p.Doors = Elevators * Floors * Columns;
-      p.Buttons = Elevators * Floors;
+      p.Doors = Elevators * Floors;
+      p.Buttons = (Elevators * Floors) + ((Columns * 5) - 2);
       p.Displays = Elevators;
       p.Pulleys = Elevators;
 
       parts.push(p);
   }
 
-  function getAddresses() public view returns (address[10] memory) {
-    return projects;
-  }
+  // function getAddresses() public view returns (address[10] memory) {
+  //   return projects;
+  // }
 
   function getProjects() public view returns (Parts[] memory) {
     return parts;
