83a8bdca5f44e37abf521afe1594db6c1650157e
Timothy J Wever <timjwever@gmail.com>
2021-04-15 21:58:38 -0400

    Solution Manufacturing modified

--- contracts/SolutionManufacturing.sol
+++ contracts/SolutionManufacturing.sol
@@ -31,7 +31,7 @@ contract SolutionManufacturing {
       // Simple logic to calculate how much material will be had according to the inputted Batteries, Columns, Elevators and Floors
       
       aluminumMaterial = (numOfElevators * numOfFloors) / numOfColumns;
-      stainlessSheetMaterial = numOfElevators * 2;
+      stainlessSheetMaterial = numOfElevators * 2;1
       steelCableMaterial = (numOfFloors * numOfElevators);
       hardwareMaterial = (numOfBatteries * 30) + (numOfColumns * 300) + (numOfElevators * 100) + (numOfFloors * 10);
       bumperMaterial = numOfColumns * 18;
@@ -41,18 +41,93 @@ contract SolutionManufacturing {
       displayLightMaterial = numOfElevators + numOfFloors;
    }
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     // Function for checking if enough material is available to create needed shafts, columns, batteries, etc.
     // Running IF ELSE Boolean statements if there is enough material to produce assembled components
 
-   function neededMaterial(int256 doorMaterial, int256 controllerMaterial, int256 panelMaterial, int256 callSignMaterial) public {
+   // function neededMaterial(int256 doorMaterial, int256 controllerMaterial, int256 panelMaterial, int256 callSignMaterial) public {
       
-   }
+   // }
 
    // Creating the Doors, Controllers, Control Panel, and Call Sign
-   
 
-   
+   mapping (string => uint256) public materials;
+
+   struct Door {
+      uint256 stainlessSheetMaterial;
+      uint256 hardwareMaterial;
+      uint256 sensorMaterial;
+   }
+
+   struct Controller {
+      uint256 wireMaterial;
+      uint256 plasticMaterial;
+      uint256 harnessMaterial;
+   }
+
+   struct ControlPanel {
+      uint256 wireMaterial;
+      uint256 plasticMaterial;
+      uint256 buttonsMaterial;
+      uint256 hardwareMaterial;
+      uint256 displayLightMaterial;
+   }
+
+   struct CallSign {
+      uint256 wireMaterial;
+      uint256 hardwareMaterial;
+      uint256 displayLightMaterial;
+   }
+
+   constructor() public {
+      materials["stainlessSheetMaterial"] = 0;
+      materials["hardwareMaterial"] = 0;
+      materials["sensorMaterial"] = 0;
+      materials["wireMaterial"] = 0;
+      materials["plasticMaterial"] = 0;
+      materials["harnessMaterial"] = 0;
+      materials["buttonsMaterial"] = 0;
+      materials["displayLightMaterial"] = 0;
+
+   }
+
+   function doors() public returns(uint numOfDoors) {
+      
+      if(materials["stainlessSheetMaterial"] >= 1 && materials["hardwareMaterial"] >= 100 && materials["sensorMaterial"] >= 1) {
+         Door memory door = Door(1, 100, 1);
+         numOfDoors = door.stainlessSheetMaterial;
+         return numOfDoors;
+
+      }
+   }
+   function controllers() public returns(uint numOfControllers) {
+
+      if(materials["wireMaterial"] >= 15 && materials["plasticMaterial"] >= 10 && materials["harnessMaterial"] >= 1) {
+         Controller memory controller = Controller(15, 10, 1);
+         numOfControllers = controller.wireMaterial;
+         return numOfControllers;
+
+      }
+   }
+   function controllPanels() public returns(uint numOfControlPanels) {
+
+      if(materials["stainlessSheetMaterial"] >= 1 && materials["hardwareMaterial"] >= 100 && materials["sensorMaterial"] >= 1) {
+         ControlPanel memory controlPanel = ControlPanel(1, 100, 1, 4, 6);
+         numOfControlPanels = controlPanel.wireMaterial;
+         return numOfControlPanels;
+
+      }
+   }
+   function callSigns() public returns(uint numOfCallSigns) {
+
+      if(materials["stainlessSheetMaterial"] >= 1 && materials["hardwareMaterial"] >= 100 && materials["sensorMaterial"] >= 1) {
+         CallSign memory callSign = CallSigns(1, 100, 1);
+         numOfCallSigns) { = materials["stainlessSheetMaterial"] /= callSign.stainlessSheetMaterial;
+         return numOfCallSigns) {;
+
+      }
+   }
    
    // Maybe a function for viewing this information on the frontend?
 
