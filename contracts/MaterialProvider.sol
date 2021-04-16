514bfc929c0f2ba3e9124f55add01646eb746c67
Tim <50148044+Tjwever@users.noreply.github.com>
2021-04-16 17:25:23 -0400

    Update MaterialProvider.sol

--- contracts/MaterialProvider.sol
+++ contracts/MaterialProvider.sol
@@ -1,61 +1,71 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 pragma experimental ABIEncoderV2;
-import "./ProjectOffice.sol";
 
-contract Material {
+contract ProjectOffice {
+  Parts[] parts;
+  address public owner;
+  // mapping(address => Projects) public projects;
 
-    //My Material object
-    struct materialObj {
-        string material;
-        uint256 reqAmount;
-    }
+  // event for EVM logging
+  event OwnerSet(address indexed oldOwner, address indexed newOwner);
 
+  // function assign(uint projectId) public returns (uint) {
+  //     require(projectId >= 0 && projectId <= 9);
 
-    //Array to store all the materials in a list and that list saved in a variable
-    materialObj[] public materialList;
-    materialObj matStruct;
+  //     projects[projectId] = msg.sender;
 
+  //     return projectId;
+  // }
 
-    //Function that will be displayed in remix
-    function pushMaterial(string memory material, uint256 reqAmount) private{
-        matStruct.material = material;
-        matStruct.reqAmount = reqAmount;
-        materialList.push(matStruct);
-    }
+  struct Parts {
+    uint256 Controllers;
+    uint256 Shafts;
+    uint256 Doors;
+    uint256 Buttons;
+    uint256 Displays;
+    uint256 Pulleys;
+  }
 
+  // event DataStored(uint256 Controllers);
 
-    //function that will handle pushing into the array above
-    function getMaterial() public view returns (materialObj[] memory material) {
-        return materialList;
-    }
+  //  modifier isOwner() {
+  //        // If the first argument of 'require' evaluates to 'false', execution terminates and all
+  //        // changes to the state and to Ether balances are reverted.
+  //        // This used to consume all gas in old EVM versions, but not anymore.
+  //        // It is often a good idea to use 'require' to check if functions are called correctly.
+  //        // As a second argument, you can also provide an explanation about what went wrong.
+  //        require(msg.sender == owner, "Caller is not owner");
+  //        _;
+  //  }
 
+   constructor() {
+       owner = msg.sender; // 'msg.sender' is sender of current call, contract deployer for a constructor
+       emit OwnerSet(address(0), owner);
+   }
 
-    //Declaring materials as variables and inside are the strings of each type of material
-    string aluminumBar ="Aluminum Bars"; 
-    string stainlessSheet ="Stainless Sheet"; 
-    string steelCable ="Steel Cable";
-    string hardware ="Hardware";
-    string interior ="Interior";
-    string display ="LED Displays";
-    string wire = "Wire";
-    string plastic = "Plastic";
+  // function changeOwner(address newOwner) public isOwner {
+  //       emit OwnerSet(owner, newOwner);
+  //       owner = newOwner;
+  // }
 
-    uint64 private index = 0;
+  function neededParts(uint256 Batteries, uint256 Columns, uint256 Elevators, uint256 Floors) public {
+      Parts memory p;
+      p.Controllers = Batteries;
+      p.Shafts = Elevators;
+      p.Doors = Elevators * Floors;
+      p.Buttons = (Elevators * Floors) + ((Columns * 5) - 2);
+      p.Displays = Elevators;
+      p.Pulleys = Elevators;
 
-    //Running calculations on the type and required amount of materials needed for this request
-    function calcMaterial(address a) public 
-    {
-        ProjectOffice.Parts[] memory parts = ProjectOffice(a).getProjects();
-        for(index;index<parts.length;index++) {  
-            pushMaterial(aluminumBar, (parts[index].Shafts *10)); 
-            pushMaterial(stainlessSheet, (parts[index].Shafts *12));
-            pushMaterial(steelCable, (parts[index].Controllers *2)); 
-            pushMaterial(hardware, (parts[index].Controllers *3));
-            pushMaterial(interior, (parts[index].Doors *4));
-            pushMaterial(display, (parts[index].Doors *1));
-            pushMaterial(wire, (parts[index].Pulleys *2));
-            pushMaterial(plastic, (parts[index].Buttons *2));
-        }
-    }
+      parts.push(p);
+  }
+
+  // function getAddresses() public view returns (address[10] memory) {
+  //   return projects;
+  // }
+
+  function getProjects() public view returns (Parts[] memory) {
+    return parts;
+  }
 }
