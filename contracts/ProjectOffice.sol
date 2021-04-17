1aa7b8192c28360414a2312ffae5e652e977d025
Ahsan Ali Syed <ahsantime1@gmail.com>
2021-04-16 22:39:46 -0400

    cleaned up

--- contracts/ProjectOffice.sol
+++ contracts/ProjectOffice.sol
@@ -5,19 +5,14 @@ pragma experimental ABIEncoderV2;
 contract ProjectOffice {
   Parts[] parts;
   address public owner;
+
   // mapping(address => Projects) public projects;
   address[10] public projects;
+
 
   // event for EVM logging
   event OwnerSet(address indexed oldOwner, address indexed newOwner);
 
-  // function assign(uint projectId) public returns (uint) {
-  //     require(projectId >= 0 && projectId <= 9);
-
-  //     projects[projectId] = msg.sender;
-
-  //     return projectId;
-  // }
 
   uint256 Controllers;
   uint256 Shafts;
@@ -25,6 +20,7 @@ contract ProjectOffice {
   uint256 Buttons;
   uint256 Displays;
   uint256 Pulleys;
+
 
   struct Parts {
     uint256 Controllers;
@@ -36,40 +32,35 @@ contract ProjectOffice {
   }
 
 
-  // event DataStored(uint256 Controllers);
-
    modifier isOwner() {
-         // If the first argument of 'require' evaluates to 'false', execution terminates and all
-         // changes to the state and to Ether balances are reverted.
-         // This used to consume all gas in old EVM versions, but not anymore.
-         // It is often a good idea to use 'require' to check if functions are called correctly.
-         // As a second argument, you can also provide an explanation about what went wrong.
-         require(msg.sender == owner, "Caller is not owner");
-         _;
-   }
+      // If the first argument of 'require' evaluates to 'false', execution terminates and all
+      // changes to the state and to Ether balances are reverted.
+      // This used to consume all gas in old EVM versions, but not anymore.
+      // It is often a good idea to use 'require' to check if functions are called correctly.
+      // As a second argument, you can also provide an explanation about what went wrong.
+      require(msg.sender == owner, "Caller is not owner");
+      _;
+    }
+
 
    constructor() public {
-       owner = msg.sender; // 'msg.sender' is sender of current call, contract deployer for a constructor
-       emit OwnerSet(address(0), owner);
-   }
+      owner = msg.sender; // 'msg.sender' is sender of current call, contract deployer for a constructor
+      emit OwnerSet(address(0), owner);
+    }
 
-  // function changeOwner(address newOwner) public isOwner {
-  //       emit OwnerSet(owner, newOwner);
-  //       owner = newOwner;
-  // }
 
   function neededParts(uint256 Batteries, uint256 Columns, uint256 Elevators, uint256 Floors) public {
-      Parts memory p;
-      p.Controllers = Batteries;
-      p.Shafts = Elevators;
-      p.Doors = Elevators * Floors;
-      /*Multiples the elevator buttons by each floor and adds the buttons outside the elevator
-      to the columns minus 1 up for the top floor and down for the first floor*/
-      p.Buttons = (Elevators * Floors) * Columns;
-      p.Displays = Elevators;
-      p.Pulleys = Elevators;
+    Parts memory p;
+    p.Controllers = Batteries;
+    p.Shafts = Elevators;
+    p.Doors = Elevators * Floors;
+    /*Multiples the elevator buttons by each floor and adds the buttons outside the elevator
+    to the columns minus 1 up for the top floor and down for the first floor*/
+    p.Buttons = (Elevators * Floors) * Columns;
+    p.Displays = Elevators;
+    p.Pulleys = Elevators;
 
-      parts.push(p);
+    parts.push(p);
   }
 
   function returnParts(uint256 Batteries, uint256 Columns, uint256 Elevators, uint256 Floors) public {
@@ -84,10 +75,6 @@ contract ProjectOffice {
 
   }
 
-
-  // function getAddresses() public view returns (address[10] memory) {
-  //   return projects;
-  // }
 
   function getProjects() public view returns (Parts[] memory) {
     return parts;
